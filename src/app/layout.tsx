67ab4adf2d1159e5ff0632466492a154a87f5f01
Your Name <shridhar@shridharlifeschool.com>
2025-05-19 18:25:16 +0100

    Fix TypeScript errors for Component and pageProps types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen bg-neutral-50">
       <main className="container mx-auto px-4 py-8 max-w-5xl">
